86c7b906aa821641a31bd404d04009c44c6cca92
houssemBa31 <houssem.bah@outlook.com>
2026-05-25 21:30:03 +0100

    Fix: Add explicit type argument to Get.to() call

--- lib/views/sign_In/widget/no_account.dart
+++ lib/views/sign_In/widget/no_account.dart
@@ -19,7 +19,7 @@ class NoAccountText extends StatelessWidget {
         ),
         GestureDetector(
           onTap: () {
-            Get.to<SignUpScreen>(const SignUpScreen());
+            Get.to<SignUpScreen>(() => const SignUpScreen());
           },
           child: const Text(
             "Sign Up",
