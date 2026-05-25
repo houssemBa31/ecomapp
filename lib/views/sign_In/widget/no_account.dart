4aa716997c62a088a7f24bb68e51f4405354fe6e
houssemBa31 <houssem.bah@outlook.com>
2026-05-25 22:26:34 +0100

    fix: Resolve dart format warnings - add type arguments and const keyword

--- lib/views/sign_In/widget/no_account.dart
+++ lib/views/sign_In/widget/no_account.dart
@@ -19,7 +19,7 @@ class NoAccountText extends StatelessWidget {
         ),
         GestureDetector(
           onTap: () {
-            Get.to<void>(SignUpScreen());
+            Get.to<SignUpScreen>(() => const SignUpScreen());
           },
           child: const Text(
             "Sign Up",
